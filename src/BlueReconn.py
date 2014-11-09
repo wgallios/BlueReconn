885839791f44e1f8e4a833f486481a0f274017eb
William Gallios <williamgallios@gmail.com>
2014-11-09 14:48:38 -0800

    updates to install process

--- src/BlueReconn.py
+++ src/BlueReconn.py
@@ -2,16 +2,22 @@
 from cement.core import backend, foundation, hook, handler
 from cement.utils.misc import init_defaults
 
+import sys
+import os
 import baseController, Install
 
-try:
-    app = foundation.CementApp('BlueReconn')
+if os.getuid() == 0:
+    try:
+        app = foundation.CementApp('BlueReconn')
 
-    handler.register(baseController.baseController)
-    handler.register(Install.Install)
+        handler.register(baseController.baseController)
+        handler.register(Install.Install)
 
-    app.setup()
+        app.setup()
 
-    app.run()
-finally:
-    app.close()
+        app.run()
+    finally:
+        app.close()
+else:
+    print("Permission denied, please run as sudo")
+    sys.exit(0)
--- src/Install.py
+++ src/Install.py
@@ -1,6 +1,15 @@
 from cement.core import controller
+from pprint import pprint
+import logging
+import shutil
+import sys
+import os
 
 class Install(controller.CementBaseController):
+    daemonLocal = '../daemon/keyboard'
+    daemonFile = '/etc/init.d/keyboard'
+
+
     class Meta:
         interface = controller.IController
         label = 'installer'
@@ -12,6 +21,31 @@ class Install(controller.CementBaseController):
 
     @controller.expose(help='Install Bluetooth connection daemon')
     def install(self):
+        self._copyDaemon()
 
-        print("Install Bluetooth Reconn Daemon")
+    def _copyDaemon(self):
+        print("Installing Bluetooth Reconn Daemon ...")
+        
+        check = self._checkDaemon()
+        
+        if not check:
+            # copies over daemon
+            shutil.copyfile(self.daemonLocal, self.daemonFile)
+            print('Done')
+        else:
+            sys.exit(1);
+
+        return True;
+
+    # checks to see if daemon file exists
+    def _checkDaemon(self):
+        if self.app.pargs.debug:
+            print("Checking if 'keyboard' daemon already exists")
+
+        if os.path.exists(self.daemonFile):
+            return True
+        else:
+            return False
+
+
 
